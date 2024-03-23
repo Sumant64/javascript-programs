a4080c1986e41f3debadb8b262a3bba442567931
Sumant Shah <sumant.shah@gigacars.com>
2024-03-24 00:00:10 +0530

    doubly linked list

--- Searching/1-linearSearch.js
+++ Searching/1-linearSearch.js
@@ -1,5 +1,6 @@
 /*
-Write a function called linearSearch which accepts an array and a value, and returns the index at which the value exists. If the value does not exist in the array, return -1.
+Write a function called linearSearch which accepts an array and a value, and returns the index at which the value exists. 
+If the value does not exist in the array, return -1.
 
 Don't use indexOf to implement this function!
 
@@ -19,7 +20,6 @@ function linearSearch(arr, value){
         if(arr[i] === value){
             return i;
         }
-        
     }
     return -1;
   }
