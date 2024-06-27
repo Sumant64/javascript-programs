497f2702c852bd5771630581ea0b41e83b67e8bd
Sumant Shah <sumant.shah@gigacars.com>
2024-06-27 14:40:52 +0530

    fabonacci recursion example dry run

--- Udemy(DSA)/Recursion/8-fib.js
+++ Udemy(DSA)/Recursion/8-fib.js
@@ -9,6 +9,17 @@ which starts with 1 and 1, and where every number thereafter is equal to the sum
 // fib(28) // 317811
 // fib(35) // 9227465
 
+/*
+    if n = 4
+    fib(3) + fib(2) 
+    fib(2) == 1
+
+    fib(3) == fib(2) + fib(1) == 1 + 1 = 2
+
+    fib(3) + fib(2) = 3
+
+*/
+
 function fib(n) {
     if(n <= 2) {
         return 1;
