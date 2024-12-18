8d5492ed39f8909e71e832bf0b7cccaac88eec4c
sumantgigacars <sumant.shah@gigacars.com>
2024-12-18 05:32:19 +0530

    tree lec 9

--- DSA(Geeks for Geeks)/Basics/Tree/lectures/9. Node at k distance.js
+++ DSA(Geeks for Geeks)/Basics/Tree/lectures/9. Node at k distance.js
@@ -23,3 +23,36 @@
  * O/P: 20 
  */
 
+/**
+ * Idea: Nodes that are at distance k from root are at distance k-1 from its children.
+ */
+
+class Node {
+    constructor(k) {
+        this.key = k;
+        this.left = null;
+        this.right = null;
+    }
+}
+
+function printkDist(root, k) {
+    if(root === null) {
+        return;
+    } 
+    if(k === 0) {
+        console.log(root.key);
+    } else {
+        printkDist(root.left, k - 1);
+        printkDist(root.right, k - 1);
+    }
+
+}
+
+let root = new Node(30);
+root.left = new Node(40);
+root.right = new Node(50);
+root.left.left = new Node(70);
+root.right.left = new Node(60);
+root.right.right = new Node(80);
+
+printkDist(root, 1)
