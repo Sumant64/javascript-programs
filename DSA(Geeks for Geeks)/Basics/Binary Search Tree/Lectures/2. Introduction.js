f960068288e05b16e2d0b7cfcc5d791eca8770cb
sumantgigacars <sumant.shah@gigacars.com>
2024-12-24 04:04:12 +0530

    introduction bst lec 2

---
+++ DSA(Geeks for Geeks)/Basics/Binary Search Tree/Lectures/2. Introduction.js
@@ -0,0 +1,36 @@
+// Binary Search Tree Introduction
+
+/**
+ * 1. For every node, keys in left side are smaller and keys in right side are greater.
+ *                        50
+ *                  30         70
+ *              10     40    60   80
+ * 2. All keys are typically considered as distant.
+ * 3. Like linked list, it is a linked data structures.
+ * 4. Implemented in C++ as map, set, multimap and multiset. And in Java
+ * is a TreeSet and TreeMap.
+ */
+
+/**
+ * Example Operations:
+ * 1. Create an empty BST
+ * 2. Insert 20, 15, 30, 40, 50, 12, 18, 35, 80, 7
+ * 
+ *                      20
+ *              15                  30
+ *          12      18                  40
+ *        7                           35   50
+ *                                             80        
+ */
+
+/**
+ * Example Operations
+ * 1. Create an empty BST
+ * 2. Insert 5, 10, 20, 30, 40
+ * 
+ *          5
+ *            10
+ *              20
+ *                 30
+ *                    40
+ */
