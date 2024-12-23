1941f54a1e5661de96ccf941bef9f0a7deb8bec3
sumantgigacars <sumant.shah@gigacars.com>
2024-12-24 03:24:16 +0530

    bst background

---
+++ DSA(Geeks for Geeks)/Basics/Binary Search Tree/Lectures/1. BST background.js
@@ -0,0 +1,18 @@
+// Binary Search Tree Background
+
+/**
+ * Time Complexity of different Operation in different dsa
+ * 
+ *                      Array(unsorted)        Array(Sorted)     Linked List     BST(balanced)       Hash Tabe
+ *  Search                  O(n)                    O(log n)        O(n)            O(log n)            O(1)
+ *  
+ *  Insert                  O(1)                    O(n)            O(1)            O(log n)            O(1)
+ * 
+ *  Delete                  O(n)                    O(n)            O(n)            O(log n)            O(1)
+ * 
+ * Find Closest             O(n)                    O(log n)        O(n)            O(log n)            O(n)
+ * 
+ * Sorted Traversal         O(n log n)               O(n)            O(nlog n)           O(n)            O(n log n)
+ *                                                                  O(n) for sorted
+ *                                                                  linked list
+ */
