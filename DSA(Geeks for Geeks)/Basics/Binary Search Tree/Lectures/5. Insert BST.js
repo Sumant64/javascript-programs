d343fce9c6642a7cf36a169bb21a3b835f6c2921
sumantgigacars <sumant.shah@gigacars.com>
2024-12-30 11:08:14 +0530

    lec 7 bst

---
+++ DSA(Geeks for Geeks)/Basics/Binary Search Tree/Lectures/5. Insert BST.js
@@ -0,0 +1,19 @@
+// Insert in BST introduction
+
+/**
+ * I/P: x = 20
+ * BST:                 10
+ *                  5       15
+ *                      12      18
+ *                                  20 (insert)
+ * 
+ * I/P: x = 10
+ *      root = null
+ * O/P      10
+ * 
+ * 
+ * o Idea:
+ * --> We will use the property of the BSt to insert. We will traverse
+ * through the node and if root comes as null we will insert the value.
+ * --> If BST is empty, then we will create root as value.
+ */
---
+++ DSA(Geeks for Geeks)/Basics/Binary Search Tree/Lectures/7. Application of BST.js
@@ -0,0 +1,12 @@
+// Application of BST
+
+/**
+ * o We will discuss on Cell Balancing BST Application
+ * 1. To maintain sorted stream of data (or sorted set of data)
+ * 2. To implement doubly ended priority queues.
+ * 3. To solve problems like:
+ *      o Count smaller/greater in a stream
+ *      o Floor/ceiling/greater/smaller in a stream
+ * 
+ * 
+ */
