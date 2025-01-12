e8377446fe65c6cfa49029c82271924903f0dc6d
sumantgigacars <sumant.shah@gigacars.com>
2025-01-12 12:42:16 +0530

    hashing lec 2

---
+++ DSA(Geeks for Geeks)/Basics/Hashing/2. Application of hashing.js
@@ -0,0 +1,14 @@
+// Application of hashing
+
+/**
+ * Application of Hashing:
+ * o Dictanaries
+ * o Database Indexing
+ * o Cryptography
+ * o Caches
+ * o Symbol Tables in Compilers/ Interpreters
+ * o Routers 
+ * o Caches
+ * o Getting data from databases 
+ * etc...
+ */
