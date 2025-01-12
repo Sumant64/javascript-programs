93c78013797ac60560f51a2dbded5ee922973753
sumantgigacars <sumant.shah@gigacars.com>
2025-01-12 12:22:09 +0530

    hashing lec 1

---
+++ DSA(Geeks for Geeks)/Basics/Hashing/1. Introduction.js
@@ -0,0 +1,11 @@
+// Introduction to hashing
+
+/**
+ * Hash Table or Hashing:
+ * o We do search, insert and delete in O(1) on average.
+ * 
+ * Not useful for:
+ * o Finding closest values (AVL tree or Red Black Tree)
+ * o Sorted Data (AVL tree or Red Black Tree)
+ * o Prefix Searching (Trie)
+ */
