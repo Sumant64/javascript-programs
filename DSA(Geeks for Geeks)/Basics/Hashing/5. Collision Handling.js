099c01e6b13b38af918fd2e17041a3c0bb64d14a
sumantgigacars <sumant.shah@gigacars.com>
2025-01-27 17:18:19 +0530

    hashing lec 5

---
+++ DSA(Geeks for Geeks)/Basics/Hashing/5. Collision Handling.js
@@ -0,0 +1,11 @@
+// Collision Handling
+
+/**
+ * o If we know keys in advance, then we can perfect hashing.
+ * o If we do not know keys, then we use one of the following.
+ *      o Chaining,
+ *      o Open Addressing
+ *          o Linear Probling
+ *          o Quadratic Probing
+ *          o Double Hashing
+ */
