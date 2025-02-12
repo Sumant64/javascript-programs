2c3c7cbaa672065988c87ac6949370b9cb9c42b3
Sumant64 <sumant1995shah@gmail.com>
2025-02-13 01:06:13 +0530

    python folder structure

---
+++ python/udemy/general Programs/6. Capitalize Word.py
@@ -0,0 +1,15 @@
+'''
+/*
+Question:
+Write a recursive function called capitalizeWords. Given an array of words, return a new array containing each word capitalized.
+*/
+
+// let words = ['i', 'am', 'learning', 'recursion'];
+// capitalizedWords(words); // ['I', 'AM', 'LEARNING', 'RECURSION']
+'''
+def capitalizeWord(arr):
+    for value in range(len(arr[])):
+        print(value)
+        
+
+capitalizeWord(['i', 'am', 'learning', 'recursion'])
