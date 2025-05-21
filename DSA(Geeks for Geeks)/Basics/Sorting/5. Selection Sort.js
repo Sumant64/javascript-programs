0fdddff6423b1000ae4a85c8580e02bb84aaf714
Sumant64 <sumant1995shah@gmail.com>
2025-05-21 18:05:20 +0530

    selection sort

---
+++ DSA(Geeks for Geeks)/Basics/Sorting/5. Selection Sort.js
@@ -0,0 +1,43 @@
+// selection sort
+
+/**
+ * 
+ * Similar to bubble sort, but instead of first placing large values
+ * into sorted position, it places small values into sorted position.
+ * 
+ * --> first loop
+ *      [5, 3, 4, 1, 2]   0
+ *       |  | 
+ *      [5, 3, 4, 1, 2]
+ *       |     |
+ *      [5, 3, 4, 1, 2]
+ *       |        |
+ *      [5, 3, 4, 1, 2]
+ *       |           |
+ *      
+ *      [1, 3, 4, 5, 2]
+ */
+
+const selectionSort = (arr) => {
+    const swap = (arr, idx1, idx2) => {
+        let temp = arr[idx1];
+        arr[idx1] = arr[idx2];
+        arr[idx2] = temp;
+    }
+
+    for(let i = 0; i < arr.length; i++) {
+        let lowest = i;
+        for(let j = i + 1; j < arr.length; j++) {
+            if(arr[lowest] > arr[j]) {
+                lowest = j;
+            }
+        }
+
+        if(i !== lowest) swap(arr, i, lowest);
+    }
+
+    return arr;
+}
+
+const res = selectionSort([0, 2, 34, 22, 10, 19, 17]);
+console.log(res);
