6bf07bdd9d08b94c6ffcdc610b20c39c458f852b
Sumant64 <sumant1995shah@gmail.com>
2025-05-21 16:19:34 +0530

    lec 3 sorting

---
+++ DSA(Geeks for Geeks)/Basics/Sorting/3. Stability in Sorting Algorithm.js
@@ -0,0 +1,19 @@
+// Stability is Sorting Algorithm
+
+/**
+ * --> Stability in Sorting Algorithm:
+ * o If the array of objects is given and we need to sort them, then after sorting 
+ * if the natural order is preserved then we say that sorting algorithm is stable.
+ * 
+ * o Example:
+ *      arr[] = [("Anil", 50), ("Ayan", 80), ("Piyush", 50), ("Ramesh", 80)]
+ * 
+ *      After stable sorting:
+ *      [("Anil", 50), ("Piyush", 50), ("Ayan", 80), ("Ramesh", 80)]
+ * 
+ *      After unstable sorting:
+ *      [("Piyush", 50), ("Anil", 50), ("Ayan", 80), ("Ramesh", 80)] --> in the original order piyush is after anil
+ * 
+ * o Stable Sorting Algorithm: Bubble sort, insertion sort, merge sort
+ * o Unstable sorting algorithm: Selection sort, Quick Sort, Heap Sort
+ */
