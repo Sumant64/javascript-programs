065c7ab767ca917077b940fe3962719b85664a29
Sumant64 <sumant1995shah@gmail.com>
2025-05-21 18:50:09 +0530

    lecture 7 sorting

---
+++ DSA(Geeks for Geeks)/Basics/Sorting/7. Merge Sort Introduction.js
@@ -0,0 +1,10 @@
+// Merge Sort Introduction
+
+/**
+ * 1. Divide and Conquer Algorithm --> (Divide, Conquer and Merge)
+ * 2. Stable Algorithm
+ * 3. Theta(n log n) Time and O(n) aux space.
+ * 4. Well suited for linked lists. Works in O(1) aux space.
+ * 5. Used in External Sorting.
+ * 6. In general for arrays, quicksort outperforms.
+ */
