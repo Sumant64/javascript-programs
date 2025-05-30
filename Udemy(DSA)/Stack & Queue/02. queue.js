141c02bf4bfb32db072ae51def244cbfd9d0e4bb
Sumant64 <sumant1995shah@gmail.com>
2025-05-30 17:25:59 +0530

    queue code changes

--- Udemy(DSA)/Stack & Queue/02. queue.js
+++ Udemy(DSA)/Stack & Queue/02. queue.js
@@ -1,47 +1,78 @@
+// Node class
 class Node {
-    constructor(value) {
-        this.value = value;
+    constructor(new_data) {
+        this.data = new_data;
         this.next = null;
     }
 }
 
-/**
- *    Queue
- *     4 --> last
- *     5
- *     6
- *     7
- *     8 --> first
- */
-
+// Queue class
 class Queue {
     constructor() {
-        this.first = null;
-        this.last = null;
-        this.size = 0;
+        this.front = this.rear = null;
     }
 
-    enqueue(val) {
-        let newNode = new Node(val);
-        if(!this.first) {
-            this.first = newNode;
-            this.last = newNode;
-        } else {
-            this.last.next = newNode;
-            this.last = newNode;
+    // Function to check if the queue is empty
+    isEmpty() {
+        return this.front === null;
+    }
+
+    // Function to add an element to the queue
+    enqueue(new_data) {
+        const new_node = new Node(new_data);
+        if (this.isEmpty()) {
+            this.front = this.rear = new_node;
+            this.printQueue();
+            return;
         }
-        return this.size++;
+        this.rear.next = new_node;
+        this.rear = new_node;
+        this.printQueue();
     }
 
+    // Function to remove an element from the queue
     dequeue() {
-        if(!this.first) return null;
-
-        let temp = this.first;
-        if(this.first === this.last){
-            this.last = null;
+        if (this.isEmpty()) {
+            return;
         }
-        this.first = this.first.next;
-        this.size--;
-        return temp.value;
+        const temp = this.front;
+        this.front = this.front.next;
+        if (this.front === null) {
+            this.rear = null;
+        }
+        this.printQueue();
+    }
+
+    // Function to print the current state of the queue
+    printQueue() {
+        if (this.isEmpty()) {
+            console.log("Queue is empty");
+            return;
+        }
+        let temp = this.front;
+        let queue_string = "Current Queue: ";
+        while (temp !== null) {
+            queue_string += temp.data + " ";
+            temp = temp.next;
+        }
+        console.log(queue_string);
     }
 }
+
+const q = new Queue();
+
+// Enqueue elements into the queue
+q.enqueue(10);
+q.enqueue(20);
+
+// Dequeue elements from the queue
+q.dequeue();
+q.dequeue();
+
+// Enqueue more elements into the queue
+q.enqueue(30);
+q.enqueue(40);
+q.enqueue(50);
+
+// Dequeue an element from the queue
+q.dequeue();
