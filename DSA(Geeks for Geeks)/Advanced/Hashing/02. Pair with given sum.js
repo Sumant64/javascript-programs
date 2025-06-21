d8264a0002c01cb2c8e24a11e0b45e2f6f308950
Sumant64 <sumant1995shah@gmail.com>
2025-06-21 19:58:39 +0530

    pair with given sum in unsorted array

---
+++ DSA(Geeks for Geeks)/Advanced/Hashing/02. Pair with given sum.js
@@ -0,0 +1,37 @@
+// Pair with Given Sum
+
+/**
+ * I/P: arr[] = [3, 2, 8, 15, -8]
+ *      sum = 17
+ * O/P: True
+ * 
+ * I/P: arr[] = [2, 1, 6, 3]
+ *      sum = 6
+ * O/P: False
+ * 
+ * I/P: arr[] = [5, 8, -3, 6]
+ *      sum = 3
+ * O/P: True
+ */
+
+/**
+ * o Naive Approach: Consider Every Pair (Time: O(n^2))
+ * o Better Approach: Sorting , then two pointer approach (Time: O(nlog n))
+ * o Efficient Approach: using hashing (Time: O(n log n);; Space: O(n))
+ */
+
+// ==========================================
+// Efficient Approach
+
+function isPairSum (arr, sum) {
+    let s = new Set();
+    for(let i = 0; i < arr.length; i++) {
+        if(s.has(sum - arr[i])) {
+            return true;
+        }
+        s.add(arr[i]);
+    }
+    return false;
+}
+
+console.log(isPairSum([10, 15, 9, 7, 5], 17));
