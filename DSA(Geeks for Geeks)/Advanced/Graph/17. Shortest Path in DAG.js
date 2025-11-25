c55f1692a96e1a671f16c596f187b412c4e55f4c
Sumant64 <sumant1995shah@gmail.com>
2025-11-25 20:13:05 +0530

    graph lecture 17

---
+++ DSA(Geeks for Geeks)/Advanced/Graph/17. Shortest Path in DAG.js
@@ -0,0 +1,124 @@
+
+
+// class MyStack {
+
+//     constructor(c) {
+//         this.cap = c;
+//         this.arr = [];
+//         this.top = -1;
+//     }
+
+//     push(x) {
+//         if (this.top === this.cap - 1) { console.log("Stack is full"); return; }
+//         this.top++;
+//         this.arr[this.top] = x;
+//     }
+
+//     pop() {
+//         if (this.top === -1) { console.log("Stack is Empty"); return -1e9; }
+//         let res = this.arr[this.top];
+//         this.top--;
+//         return res;
+//     }
+
+//     peek() {
+//         if (this.top === -1) { console.log("Stack is Empty"); return -1e9; }
+//         return this.arr[this.top];
+//     }
+
+//     size() {
+//         return (this.top + 1);
+//     }
+
+//     isEmpty() {
+//         return (this.top === -1);
+//     }
+// }
+
+
+class AdjListNode {
+    constructor(v, w) {
+        this.v = v;
+        this.weight = w;
+    }
+
+    getV() {
+        return this.v;
+    }
+
+    getWeight() {
+        return this.weight;
+    }
+}
+
+class Graph {
+    constructor(V) {
+        this.V = V;
+        this.adj = new Array(V);
+        for (let i = 0; i < V; i++) {
+            this.adj[i] = [];
+        }
+    }
+
+    addEdge(u, v, weight) {
+        this.adj[u].push(new AdjListNode(v, weight));
+    }
+
+    topologicalSortUtil(v, visited, Stack) {
+        visited[v] = true;
+        for (const node of this.adj[v]) {
+            if (!visited[node.getV()]) {
+                this.topologicalSortUtil(node.getV(), visited, Stack);
+            }
+        }
+        Stack.push(v);
+    }
+
+    shortestPath(s) {
+        const INF = Number.POSITIVE_INFINITY;
+        const Stack = [];
+        const dist = new Array(this.V).fill(INF);
+        const visited = new Array(this.V).fill(false);
+        for (let i = 0; i < this.V; i++) {
+            if (!visited[i]) {
+                this.topologicalSortUtil(i, visited, Stack);
+            }
+        }
+
+        dist[s] = 0;
+
+        while (Stack.length > 0) {
+            const u = Stack.pop();
+            if (dist[u] !== INF) {
+                for (const node of this.adj[u]) {
+                    if (dist[node.getV()] > dist[u] + node.getWeight()) {
+                        dist[node.getV()] = dist[u] + node.getWeight();
+                    }
+                }
+            }
+        }
+
+        for (let i = 0; i < this.V; i++) {
+            if (dist[i] === INF) {
+                console.log("INF");
+            }
+            else {
+                console.log(dist[i]);
+            }
+        }
+
+    }
+}
+
+const g = new Graph(6);
+g.addEdge(0, 1, 2);
+g.addEdge(0, 4, 1);
+g.addEdge(1, 2, 3);
+g.addEdge(4, 2, 2);
+g.addEdge(4, 5, 4);
+g.addEdge(2, 3, 6);
+g.addEdge(5, 3, 1);
+
+const s = 0;
+console.log("Following are shortest distances from source", s);
+g.shortestPath(s);
