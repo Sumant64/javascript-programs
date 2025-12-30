ab55b3e35eb2b508e2b85233507eadb9ee564b54
Sumant64 <sumant1995shah@gmail.com>
2025-12-30 16:15:47 +0530

    lect 2 queue

---
+++ DSA(Geeks for Geeks)/Basics/Queue/02. Application of Queue.js
@@ -0,0 +1,12 @@
+// Application of Queue
+
+/**
+ * --> Application of Queue:
+ * o Single Resource and Multiple Consumers
+ * o Synchronization between slow and fast devices.
+ * o In Operating Systems (Semaphores, FCFS Scheduling, Spooling, buffers
+ *   for devices like keyboards).
+ * o In Computer Networks (Routers, Switches and Mail Queues.)
+ * o Variations: Dequeue, Priority Queue and Doubly Ended Priority Queue.
+ */
+
